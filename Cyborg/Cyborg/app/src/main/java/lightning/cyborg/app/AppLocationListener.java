149b895d389fdcfa894454df4eeb2de416564be5
nashwan nouri <nashwan.nouri@kcl.ac.uk>
2016-03-12 20:29:15 +0000

    methods for retrieving longitude and latidude

--- Cyborg/Cyborg/app/src/main/java/lightning/cyborg/app/AppLocationListener.java
+++ Cyborg/Cyborg/app/src/main/java/lightning/cyborg/app/AppLocationListener.java
@@ -17,6 +17,8 @@ public class AppLocationListener implements LocationListener {
     private static LocationManager locationManager;
     private static LocationListener locationListener;
     private static String stringUpdateGPS;
+    private static double longitude;
+    private static double latidude;
     private LocationManager tempoLocationManager;
 
     @SuppressWarnings("ResourceType")
@@ -29,6 +31,8 @@ public class AppLocationListener implements LocationListener {
     @SuppressWarnings("ResourceType")
     @Override
     public void onLocationChanged(Location location) {
+        latidude=location.getLatitude();
+        longitude =location.getLongitude();
         stringUpdateGPS = location.getLatitude() + ", " + location.getLongitude();
         Log.d("AppLocationListener", "onLocationChanged: " + stringUpdateGPS);
         tempoLocationManager.removeUpdates(this);
@@ -60,4 +64,11 @@ public class AppLocationListener implements LocationListener {
         tempoLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 40, 0, this);
     }
 
+    public static double getLongitude() {
+        return longitude;
+    }
+
+    public static double getLatidude() {
+        return latidude;
+    }
 }
