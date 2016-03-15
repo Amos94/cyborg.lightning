a0bd1f172ea121bf8e74df17ed90b8e7a26b68bd
010100111001 <austin.lautissier@kcl.ac.uk>
2016-03-15 00:49:46 +0000

    Mostly done with discovery

--- Cyborg/Cyborg/app/src/main/java/lightning/cyborg/fragment/DiscoveryFragment.java
+++ Cyborg/Cyborg/app/src/main/java/lightning/cyborg/fragment/DiscoveryFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,11 +13,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 
-import lightning.cyborg.R;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 
-/**
- * Created by Lewis on 21/02/2016.
- */
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import lightning.cyborg.R;
+import lightning.cyborg.app.MyApplication;
 
 public class DiscoveryFragment extends Fragment {
 
@@ -25,6 +34,8 @@ public class DiscoveryFragment extends Fragment {
     private ListView friendlist;
     private ArrayAdapter adapter;
     private Button searchButton;
+    private String[] matchedUsers;
+    private String[][] matchedUserInfo;
 
     public DiscoveryFragment() {
         // Required empty public constructor
@@ -40,7 +51,6 @@ public class DiscoveryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-
         // Inflate the layout for this fragment
         this.inflatedview = inflater.inflate(R.layout.discovery_fragment, container, false);
 
@@ -50,45 +60,147 @@ public class DiscoveryFragment extends Fragment {
 
         friendlist = (ListView) inflatedview.findViewById(R.id.lvFriends);
         search = (EditText) inflatedview.findViewById(R.id.searchMatches);
-        //searchButton = (Button) inflatedview.findViewById(R.id.searchButton);
 
-//        Button searchButton = (Button) inflatedview.findViewById(R.id.searchButton);
-//        searchButton.setOnClickListener(new View.OnClickListener() {
+//        search.addTextChangedListener(new TextWatcher() {
 //            @Override
-//            public void onClick(final View view) {
-//                adapter = new ArrayAdapter(getContext(), R.layout.support_simple_spinner_dropdown_item, lists);
-//                friendlist.setAdapter(adapter);
+//            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+//                adapter.getFilter().filter(s);
+//
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//                adapter.getFilter().filter(s);
+//
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable s) {
+//                adapter.getFilter().filter(s);
+//
 //            }
 //        });
 
         adapter = new ArrayAdapter(getContext(), R.layout.support_simple_spinner_dropdown_item, lists);
         friendlist.setAdapter(adapter);
 
-        search.addTextChangedListener(new TextWatcher() {
+        searchButton = (Button)inflatedview.findViewById(R.id.searchButton);
+        searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                adapter.getFilter().filter(s);
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                adapter.getFilter().filter(s);
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                adapter.getFilter().filter(s);
-
+            public void onClick(View v) {
+                discover(v);
             }
         });
 
-        adapter = new ArrayAdapter(getContext(), R.layout.support_simple_spinner_dropdown_item, lists);
-        friendlist.setAdapter(adapter);
-
         return inflatedview;
 
+    }
+
+    private void discover(View v){
+        final String url = "http://nashdomain.esy.es/interests_get_matched.php";
+        final String filtered = search.getText().toString().replaceAll(", ",",").replaceAll(" ,",",").toLowerCase();
+
+        //parameters to post to php file
+        final Map<String, String> params = new HashMap<String, String>();
+        params.put("interests", filtered);
+        params.put("userID", "69");
+
+        //request to insert the user into the mysql database using php
+        StringRequest request = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+
+                            //clips the brackets off of the php array
+                            String users = jsonResponse.getString("users").replaceAll("\"","");
+                            users = users.substring(1, users.length() - 1);
+                            matchedUsers = users.split(",");
+
+                            populateDiscovery(0, 5);
+
+                            boolean success = jsonResponse.getString("success").equals("1");
+                            String message = jsonResponse.getString("message");
+                        }
+                        catch (JSONException e) {
+                            e.printStackTrace();
+                            Log.d("JSON failed to parse: ", response);
+                        }
+                    }
+                }, new Response.ErrorListener(){
+
+            @Override
+            public void onErrorResponse(VolleyError error){
+                Log.d("VolleyError at url ", url);
+            }
+        }
+        ){
+            //Parameters inserted
+            @Override
+            protected Map<String, String> getParams()
+            {
+                return params;
+            }
+        };
+        //put the request in the static queue
+        MyApplication.getInstance().addToRequestQueue(request);
+    }
+
+    private void populateDiscovery(int from, int to){
+        final String url = "http://nashdomain.esy.es/users_get.php";
+
+        if (from < 0){
+            return;
+        }
+        if (to > matchedUsers.length){
+            to = matchedUsers.length;
+        }
+
+        for(int i = from; i < to; i++){
+            Log.d("looking for user ", matchedUsers[i]);
+
+            //parameters to post to php file
+            final Map<String, String> params = new HashMap<String, String>();
+            params.put("userID", matchedUsers[i]);
+
+            //request to insert the user into the mysql database using php
+            StringRequest request = new StringRequest(Request.Method.POST, url,
+                    new Response.Listener<String>() {
+
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                JSONObject jsonResponse = new JSONObject(response);
+
+                                JSONObject user = jsonResponse.getJSONObject("user");
+                                boolean success = jsonResponse.getString("success").equals("1");
+                                String message = jsonResponse.getString("message");
+                            }
+                            catch (JSONException e) {
+                                e.printStackTrace();
+                                Log.d("JSON failed to parse: ", response);
+                            }
+                        }
+                    }, new Response.ErrorListener(){
+
+                @Override
+                public void onErrorResponse(VolleyError error){
+                    Log.d("VolleyError at url ", url);
+                }
+            }
+            ){
+                //Parameters inserted
+                @Override
+                protected Map<String, String> getParams()
+                {
+                    return params;
+                }
+            };
+            //put the request in the static queue
+            MyApplication.getInstance().addToRequestQueue(request);
+        }
     }
 
     //@Override
