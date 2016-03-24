bbde2df0420b4da21a351a1882acf1e0a1420efe
gurunish <nishant.gurung@kcl.ac.uk>
2016-03-24 19:02:25 +0000

    fragment comments added

--- Cyborg/Cyborg/app/src/main/java/lightning/cyborg/fragment/DateDialog.java
+++ Cyborg/Cyborg/app/src/main/java/lightning/cyborg/fragment/DateDialog.java
@@ -10,13 +10,26 @@ import android.widget.EditText;
 
 import java.util.Calendar;
 
+/**
+ * This class allows user to interact with a Dialog calendar to input date of birth
+ * Created by Team Cyborg Lightning
+ */
 public class DateDialog extends DialogFragment
         implements DatePickerDialog.OnDateSetListener{
-
     EditText etDate;
+
+    /**
+     * Default constructor for class
+     * @param view the container that calls this method
+     */
     public DateDialog(View view) {
         etDate = (EditText) view;
     }
+
+    /**
+     * Default method that is ran by app
+     * @param savedInstanceState  where user previously left off
+     */
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the current time as the default values for the picker
@@ -28,9 +41,13 @@ public class DateDialog extends DialogFragment
         return new DatePickerDialog(getActivity(), this, year, month, day);
     }
 
-
-
-
+    /**
+     * Method that helps to format the date that was chosen
+     * @param view the container that calls this class object
+     * @param year year that was chosen in the Calendar
+     * @param monthIn month that was chosen in the Calendar
+     * @param day day that was chosen in the Calendar
+     */
     @Override
     public void onDateSet(DatePicker view, int year, int monthIn, int day) {
 
@@ -54,10 +71,6 @@ public class DateDialog extends DialogFragment
         }else
             //show to the selected date in the text box
             date = String.format("%d-%d-%d", year, month, day);
-
         etDate.setText(date);
-
     }
-
-
 }
